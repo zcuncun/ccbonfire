bd817c20f6f841cfd9b358279f76da25d716ec1e
zcuncun <zcuncun@outlook.com>
2022-02-06 23:08:10 +0800

    rt

--- ccbonfire/runner/base_runner.py
+++ ccbonfire/runner/base_runner.py
@@ -5,24 +5,130 @@
 base_runner.py
 """
 
-import argparse, logging
 import torch
+import time
 
 
-class BaseRunner():
-    def __init__(self, cfg):
-        is_gpu = torch.cuda.is_available()
-        model = cfg["model"]["type"](cfg["model"]["args"])
+class TrainRunner:
+    def __init__(
+        self,
+        model,
+        train_dataloader,
+        val_dataloader,
+        loss_fn,
+        loss_collate_fn,
+        eval_fn,
+        eval_collate_fn,
+    ):
+        self.model = model
+        self.train_dataloader = train_dataloader
+        self.val_dataloader = val_dataloader
+        self.loss_fn = loss_fn
+        self.loss_collate_fn = loss_collate_fn
+        self.eval_fn = eval_fn
+        self.eval_collate_fn = eval_collate_fn
+        self.hooks = {
+            "before_run": [],
+            "before_train_epoch": [],
+            "after_train_epoch": [],
+            "before_train_iter": [],
+            "after_train_iter": [],
+            "before_val_epoch": [],
+            "after_val_epoch": [],
+        }
+        self.record = {
+            "epoch": 0,
+            "step": 0,
+            "loss": [],
+            "val_loss": [],
+            "val_eval": [],
+            "best_eval": None,
+        }
+        self.meta = {}
 
-    def run():
-        
+    def call_hook(self, stage):
+        for hook in self.hooks[stage]:
+            hook(self)
+
+    def run(
+        self,
+    ):
+        self.call_hook("before_run")
+        while self.record["epoch"] < self._max_epochs:
+            self.call_hook("before_train_epoch")
+            for data in self.train_dataloader:
+                self.call_hook("before_train_forward")
+                output = self.model(data)
+                self.loss = self.loss_fn(output, data)
+                self.call_hook("after_train_forward")
+            self.call_hook("after_train_epoch")
+
+            self.call_hook("before_val_epoch")
+            self.model.eval()
+            val_losses = []
+            val_evals = []
+            for data in self.val_dataloader:
+                output = self.model(data)
+                val_loss = self.loss_fn(output, data)
+                val_losses.append(val_loss)
+                val_eval = self.eval_fn(output, data)
+                val_evals.append(val_eval)
+            self.record["val_loss"].append(self.loss_collate_fn(val_losses))
+            self.record["val_eval"].append(self.eval_collate_fn(val_evals))
+
+            # 经过一个 epoch 迭代后调用
+            self.call_hook("after_val_epoch")
+
+        # 运行完成前调用
+        self.call_hook("after_run")
 
 
-        
+class EvalRunner:
+    def __init__(self, model, eval_data, eval_fn, eval_collate_fn):
+        """Runner for testing.
+        Args:
+            model : model to evaluate.
+            test_data : data to evaluate.
+            eval_fn: evaluate function for batch
+            eval_collate_fn : collate eval result from batch outputs
+        """
+        self.model = model
+        self.eval_data = eval_data
+        self.eval_fn = eval_fn
+        self.eval_collate_fn = eval_collate_fn
+
+    def run(self):
+        self.model.eval()
+        eval_reses = []
+        for data in self.eval_data:
+            output = self.model(data["input"])
+            eval_res = self.eval_fn(output, data["label"])
+            eval_reses.append(eval_res)
+        final_res = self.eval_collate_fn(eval_reses)
+        return final_res
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='OCRTrain launch')
-    parser.add_argument('--cfg', required=True, type=str, help='config path')
-    args = parser.parse_args()
-    main(args)
+class TestRunner:
+    def __init__(self, model, test_data, output_collate_fn):
+        """Runner for testing.
+        Args:
+            model : model to test.
+            test_data : data to test.
+            collate_fn : collate output from batch outputs
+        """
+        self.model = model
+        self.test_data = test_data
+        self.output_collate_fn = output_collate_fn
+
+    def run(self):
+        self.model.eval()
+        outputs = []
+        for data in self.test_data:
+            output = self.model(data["input"])
+            outputs.append(output)
+        final_output = self.output_collate_fn(outputs)
+        return final_output
+
+
+if __name__ == "__main__":
+    pass
---
+++ ccbonfire/utils/parallel.py
@@ -0,0 +1,76 @@
+import torch
+import torch.distributed as dist
+
+_LOCAL_PROCESS_GROUP = None
+"""
+A torch process group which only includes processes that on the same machine as the current process.
+This variable is set when processes are spawned by `launch()` in "engine/launch.py".
+"""
+
+
+def get_world_size() -> int:
+    if not dist.is_available():
+        return 1
+    if not dist.is_initialized():
+        return 1
+    return dist.get_world_size()
+
+
+def get_rank() -> int:
+    if not dist.is_available():
+        return 0
+    if not dist.is_initialized():
+        return 0
+    return dist.get_rank()
+
+
+def get_local_rank() -> int:
+    """
+    Returns:
+        The rank of the current process within the local (per-machine) process group.
+    """
+    if not dist.is_available():
+        return 0
+    if not dist.is_initialized():
+        return 0
+    assert (
+        _LOCAL_PROCESS_GROUP is not None
+    ), "Local process group is not created! Please use launch() to spawn processes!"
+    return dist.get_rank(group=_LOCAL_PROCESS_GROUP)
+
+
+def get_local_size() -> int:
+    """
+    Returns:
+        The size of the per-machine process group,
+        i.e. the number of processes per machine.
+    """
+    if not dist.is_available():
+        return 1
+    if not dist.is_initialized():
+        return 1
+    return dist.get_world_size(group=_LOCAL_PROCESS_GROUP)
+
+
+def is_main_process() -> bool:
+    return get_rank() == 0
+
+
+def synchronize():
+    """
+    Helper function to synchronize (barrier) among all processes when
+    using distributed training
+    """
+    if not dist.is_available():
+        return
+    if not dist.is_initialized():
+        return
+    world_size = dist.get_world_size()
+    if world_size == 1:
+        return
+    if dist.get_backend() == dist.Backend.NCCL:
+        # This argument is needed to avoid warnings.
+        # It's valid only for NCCL backend.
+        dist.barrier(device_ids=[torch.cuda.current_device()])
+    else:
+        dist.barrier()
